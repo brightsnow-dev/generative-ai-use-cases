e2ddd72ff6dead604930007cb36cf14bc7bb3f1e
brightsnow-dev <jamesrusher248@gmail.com>
2025-07-12 14:50:08 -0500

    update browser extension proxy store

--- browser-extension/src/app/proxyStore.ts
+++ browser-extension/src/app/proxyStore.ts
@@ -1,6 +1,7 @@
 import { applyMiddleware, Store } from '@eduardoac-skimlinks/webext-redux';
 import thunkMiddleware from 'redux-thunk';
 
+
 const middlewares = [thunkMiddleware];
 
 export const proxyStore = applyMiddleware(new Store(), ...middlewares);
